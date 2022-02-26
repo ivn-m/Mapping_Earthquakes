17ce42cf7ee861c9656514f2c05d4490f8f9fd28
Ivan Montoya <montoyaivan.94@gmail.com>
2022-02-26 16:11:12 -0500

    Adding earthquake data to map

--- Earthquakes_past7days/static/js/logicStep1.js
+++ Earthquakes_past7days/static/js/logicStep1.js
@@ -32,20 +32,8 @@ let map = L.map('mapid', {
 // Pass our map layers into our layers control and add the layers control to the map.
 L.control.layers(baseMaps).addTo(map);
 
-// // Then we add our 'graymap' tile layer to the map.
-// streets.addTo(map);
-
-// Accessing the Toronto neighborhoods GeoJSON URL.
-let torontoHoods = "https://raw.githubusercontent.com/ivn-m/Mapping_Earthquakes/main/torontoNeighborhoods.json";
-
-let myStyle = {
-  color:"#ffffa1",
-  weight: 1
-}
-
-// Grabbing our GeoJSON data.
+// Retrieve the earthquake GeoJSON data.
 d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson").then(function(data) {
-  console.log(data);
   // Creating a GeoJSON layer with the retrieved data.
   L.geoJSON(data).addTo(map);
-  });
+});
